5a0d39d263005d584633d05af6dd9232323c883c
Xris Xtrom <crekstroem@gmail.com>
2025-06-22 22:53:46 -0500

    done with UI for now

--- lib/screens/main_navigation_screen.dart
+++ lib/screens/main_navigation_screen.dart
@@ -44,64 +44,12 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       resizeToAvoidBottomInset: false,
-      body: Column(
-        children: [
-          // Custom App Bar with page indicator
-          Container(
-            height: 60 + MediaQuery.of(context).padding.top,
-            padding: EdgeInsets.only(
-              left: AppConfig.standardPadding,
-              right: AppConfig.standardPadding,
-              top: MediaQuery.of(context).padding.top,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Page indicators
-                Row(
-                  children: List.generate(_screens.length, (index) {
-                    return Container(
-                      margin: EdgeInsets.symmetric(horizontal: AppConfig.smallPadding),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            _screenTitles[index],
-                            style: TextStyle(
-                              color: _currentIndex == index ? Colors.white : Colors.grey,
-                              fontSize: _currentIndex == index ? 16 : 14,
-                              fontWeight: _currentIndex == index ? FontWeight.bold : FontWeight.normal,
-                            ),
-                          ),
-                          SizedBox(height: AppConfig.smallPadding),
-                          Container(
-                            width: 6,
-                            height: 6,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: _currentIndex == index ? Colors.white : Colors.transparent,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
-                ),
-              ],
-            ),
-          ),
-          // Page view
-          Expanded(
-            child: PageView(
-              controller: _pageController,
-              onPageChanged: _onPageChanged,
-              children: _screens.map((screen) => Padding(
-                padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
-                child: screen,
-              )).toList(),
-            ),
-          ),
-        ],
+      body: SafeArea(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: _onPageChanged,
+          children: _screens,
+        ),
       ),
     );
   }
